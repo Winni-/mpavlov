2c7768a4cc37d30f8d6f6d17048d207f4ce26cca
Ivan Sorokin <sorokindev@gmail.com>
2014-02-25 20:50:00 +0200

    Rename crudHandler to crudHandlers

--- handlers/entities.js
+++ handlers/entities.js
@@ -8,6 +8,6 @@ var mongoose = require('../libs/mongoose');
 // Выставляем modelName
 var modelName = 'entities';
 // Подгружаем стандартные метода для CRUD документов
-var handlers = require('../libs/crudHandler')(modelName);
+var handlers = require('../libs/crudHandlers')(modelName);
 
 module.exports = handlers;
