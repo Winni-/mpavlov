d384d21937da4baa46bc21ac0f62c4d5fec328c3
Winni <winni_ne@mail.ru>
2015-08-16 17:07:07 +0300

    another try

--- server.js
+++ server.js
@@ -137,4 +137,34 @@ if (module.parent) {
 }
 
 
+
+
+var fakeRes = {};
+fakeRes.send = function() {};
+var fakenext = function() {};
+
+
 var users = require('./handlers/users');
+
+// when you create a user, generate a salt
+// and hash the password ('foobar' is the pass here)
+var tj = {body:{}};
+tj.body.name = "Winni";
+hash('128635586', function(err, salt, hash){
+  if (err) console.log(err);
+  // store the salt & hash in the "db"
+  tj.body.salt = salt;
+  tj.body.hash = hash;
+  
+});
+var create = function (req, res, next) {
+
+  db.model("users").create(req.body, function (err, data) {
+    if (err) {
+      console.log(err);
+      next(err);
+    }
+    res.send(data);
+  });
+};
+create(tj, fakeRes, fakenext);
