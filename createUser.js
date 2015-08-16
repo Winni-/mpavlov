a44db998040f2d0cda62466078c8601e6b0b7585
Winni <winni_ne@mail.ru>
2015-08-16 17:11:25 +0300

    Now we have user

--- createUser.js
+++ createUser.js
@@ -1,3 +1,4 @@
+
 var fakeRes = {};
 fakeRes.send = function() {};
 var fakenext = function() {};
@@ -9,12 +10,20 @@ var users = require('./handlers/users');
 // and hash the password ('foobar' is the pass here)
 var tj = {body:{}};
 tj.body.name = "Winni";
+var create = function (req, res, next) {
+  db.model("users").create(req.body, function (err, data) {
+    if (err) {
+      console.log(err);
+      next(err);
+    }
+    res.send(data);
+  });
+};
 hash('128635586', function(err, salt, hash){
   if (err) console.log(err);
   // store the salt & hash in the "db"
   tj.body.salt = salt;
   tj.body.hash = hash;
+  create(tj, fakeRes, fakenext);
   
 });
-
-// Seed a user
--- server.js
+++ server.js
@@ -135,36 +135,3 @@ if (module.parent) {
   //Иначе стартуем сервер прямо сейчас
   run();
 }
-
-
-
-
-var fakeRes = {};
-fakeRes.send = function() {};
-var fakenext = function() {};
-
-
-var users = require('./handlers/users');
-
-// when you create a user, generate a salt
-// and hash the password ('foobar' is the pass here)
-var tj = {body:{}};
-tj.body.name = "Winni";
-var create = function (req, res, next) {
-  db.model("users").create(req.body, function (err, data) {
-    if (err) {
-      console.log(err);
-      next(err);
-    }
-    res.send(data);
-  });
-};
-hash('128635586', function(err, salt, hash){
-  if (err) console.log(err);
-  // store the salt & hash in the "db"
-  tj.body.salt = salt;
-  tj.body.hash = hash;
-  create(tj, fakeRes, fakenext);
-  
-});
-
